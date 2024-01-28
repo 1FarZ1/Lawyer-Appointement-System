cbfb3b75505a3bef1b0b3c2355767b014b30c3e0
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2024-01-28 06:00:38 +0100

    Update .gitignore and database configuration

--- app/config/config.py
+++ app/config/config.py
@@ -1,6 +1,20 @@
-# from dotenv import load_dotenv
-
-# load_dotenv()
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # CLIENT_ID = os.environ.get("CLIENT_ID")
 # CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+
+
+
+## db config 
+
+DB_HOST = os.environ.get("DB_HOST")
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+DB_NAME = os.environ.get("DB_NAME")
+DB_PORT = os.environ.get("DB_PORT")
+
+
+
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://" + DB_USER + ":" + DB_PASS + "@" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME
